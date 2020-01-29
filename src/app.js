8643844c2964a2c424e0562342a17b3ad5b4e6dd
evaveskova <evaveskova@gmail.com>
2020-01-29 13:44:43 +0100

    fetch api and add event listener to form

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 import './style.css';
+// import getWeather from './api.js';
+const getData = (e) => {
+  e.preventDefault();
+  console.log('test');
+  const city = document.getElementById('city-input').value;
+  const API_KEY = '2b973a9ddd64acbb314bdaa3d2f4ec6b';
+  const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`;
+  fetch(url).then( response => {
+      console.log(response);
+  })
+}
+const inputForm = document.getElementById('input-form');
+inputForm.addEventListener('submit', getData, false);
+
+
+
+// getWeather();
