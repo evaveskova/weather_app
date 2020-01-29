53d4045abb407545a56e760f321e50343c326e58
evaveskova <evaveskova@gmail.com>
2020-01-29 13:51:08 +0100

    convert response to json and get weather and main data from response

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ const getData = (e) => {
   const city = document.getElementById('city-input').value;
   const API_KEY = '2b973a9ddd64acbb314bdaa3d2f4ec6b';
   const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`;
-  fetch(url).then( response => {
-      console.log(response);
-  })
+  fetch(url)
+    .then( response => response.json())
+    .then( ({ weather, main}) => {
+      console.log(weather, main)
+    })
 }
 const inputForm = document.getElementById('input-form');
 inputForm.addEventListener('submit', getData, false);
