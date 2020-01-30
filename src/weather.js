fb016516bf5a6c9a7a65314725c9120038a10f2c
evaveskova <evaveskova@gmail.com>
2020-01-30 13:46:21 +0100

    add default background image

--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,7 @@
-
 const setBackground = (weatherType) => {
   const body = document.getElementById('body');
-  body.style.background = `#000000 url(images/${weatherType}.jpg) no-repeat center center fixed`;
-  body.style.backgroundSize = 'cover';
+    body.style.background = `#000000 url(images/${weatherType}.jpg) no-repeat center center fixed`;
+    body.style.backgroundSize = 'cover';
 }
 
 export default setBackground;
